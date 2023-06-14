d56602044102eb4646329b5161be6b6cd27b1194
Alaa Obeid <alaaobed88@gmail.com>
2023-06-14 20:35:53 +0300

    added React strict mode to the render method

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import ReactDom from "react-dom";
 import App from "./App";
 import "./index.css";
+import React from "react";
 
 const root = ReactDom.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
