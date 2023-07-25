f7f4e8ce67187228f7c6b617063e2451060b4029
Alaa Obeid <alaaobed88@gmail.com>
2023-07-25 23:31:38 +0300

    added backend and front end in skills section

---
+++ src/components/Skills/BackEnd.js
@@ -0,0 +1,73 @@
+import React from 'react'
+
+const BackEnd = () => {
+  return (
+    <div className="skills-content">
+    <h3 className="skills-title">BackEnd Developer</h3>
+    <div className="skills-box">
+      <div className="skills-group">
+        <div className="skills-data">
+          <i class="bx bx-badge-check"></i>
+          <div>
+            <h3 className="skills-name"></h3>
+            <span className="skills-level"></span>
+          </div>
+        </div>
+        <div className="skills-data">
+          <i class="bx bx-badge-check"></i>
+          <div>
+            <h3 className="skills-name"></h3>
+            <span className="skills-level"></span>
+          </div>
+        </div>
+        <div className="skills-data">
+          <i class="bx bx-badge-check"></i>
+          <div>
+            <h3 className="skills-name"></h3>
+            <span className="skills-level"></span>
+          </div>
+        </div>
+        <div className="skills-data">
+          <i class="bx bx-badge-check"></i>
+          <div>
+            <h3 className="skills-name"></h3>
+            <span className="skills-level"></span>
+          </div>
+        </div>
+      </div>
+      <div className="skills-group">
+        <div className="skills-data">
+          <i class="bx bx-badge-check"></i>
+          <div>
+            <h3 className="skills-name"></h3>
+            <span className="skills-level"></span>
+          </div>
+        </div>
+        <div className="skills-data">
+          <i class="bx bx-badge-check"></i>
+          <div>
+            <h3 className="skills-name"></h3>
+            <span className="skills-level"></span>
+          </div>
+        </div>
+        <div className="skills-data">
+          <i class="bx bx-badge-check"></i>
+          <div>
+            <h3 className="skills-name"></h3>
+            <span className="skills-level"></span>
+          </div>
+        </div>
+        <div className="skills-data">
+          <i class="bx bx-badge-check"></i>
+          <div>
+            <h3 className="skills-name"></h3>
+            <span className="skills-level"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default BackEnd
---
+++ src/components/Skills/Skills.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Skills = () => {
+  return (
+    <section className="skills section" id="skills">
+      <h2>Skills</h2>
+      <span>My technical level</span>
+      <div className="skills-container container grid"></div>
+    </section>
+  );
+};
+
+export default Skills;
